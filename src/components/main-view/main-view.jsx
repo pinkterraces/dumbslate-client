01ea3432f85de2871f0dd648606cd6c775bf1307
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2023-02-08 14:40:58 +0100

    Added login and signup views to main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view"
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = JSON.parse(localStorage.getItem("token"));
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null); 
 
   useEffect(() => {
-    fetch("https://dumbslate.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+    fetch("http://localhost:8080/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then((res) => res.json())
       .then((data) => {
-        console.log("data: ", data);
+        //console.log("data: ", data);
         const allFilmsApi = data.map((movies) => {
           return {
             id: movies._id,
@@ -24,7 +34,21 @@ export const MainView = () => {
         });
         setMovies(allFilmsApi);
       });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView 
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+            }} 
+        />
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return ( 
@@ -39,7 +63,18 @@ export const MainView = () => {
         }
 
   if (movies.length === 0) {
-    return <div>This list doesn't contain any movies.</div>
+    return (
+      <>
+        <div>This list doesn't contain any movies.</div>
+        <button
+              onClick={() => {
+                setUser(null)
+                setToken(null)
+                localStorage.clear();
+              }}
+            >Log Out</button>
+      </>
+    )
   }
 
   return (
@@ -53,6 +88,16 @@ export const MainView = () => {
           }}
         />
       ))}
+        <button
+          onClick={() => {
+            setUser(null)
+            setToken(null)
+            localStorage.clear();
+          }}
+        >Log Out</button>
     </div>
   );
+
+  
+
 };
